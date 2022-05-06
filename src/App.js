b166fa816c0604360242e4e61550e1198ae4876b
Zahid Khawaja <zahidkhawaja@icloud.com>
2022-05-06 15:39:36 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
+import CircularProgress from '@mui/material/CircularProgress';
 
 function App() {
+
+  const [imageFile, setImageFile] = useState()
+  const [previewImage, setPreviewImage] = useState()
+  const [resultReady, setResultReady] = useState(false);
+  const [result, setResult] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const changeHandler = e => {
+    setImageFile(e.target.files[0]);
+  }
+
+  const handleSubmission = () => {
+
+    setLoading(true);
+
+    const formData = new FormData();
+
+    formData.append("image", imageFile);
+
+    fetch(
+      "https://nft-classifier.herokuapp.com/classify",
+      {
+        method: "POST",
+        body: formData,
+      }
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        console.log("Success: ", result);
+        setLoading(false);
+        setResultReady(true);
+        setResult(result)
+        setPreviewImage(URL.createObjectURL(imageFile))
+      })
+      .catch((error) => {
+        console.error("Error: ", error.error);
+      });
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>NFT Classifier</h1>
       </header>
+      <div className = "upload-section">
+      <input type = "file" name = "file" accept="image/*" onChange = {changeHandler} required />
+      <div className = "upload-button">
+        <button onClick = {handleSubmission}>Upload</button>
+      </div>
+      </div>
+      <div className = "result-section">
+        {loading ? <CircularProgress /> : null}
+        {resultReady ? <div className = "results-block"> <img src = {previewImage} className = "preview" alt = "NFT"></img> <p>{result.success}</p></div> : null} 
+      </div>
     </div>
   );
 }
